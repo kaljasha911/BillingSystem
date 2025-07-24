0e98b03801e3dfb078bc46988dd8745856855c59
Jashan <jashanjot381@gmail.com>
2025-07-23 23:37:18 -0400

    Basic UI without functionality

--- Assignment03/ContentView.swift
+++ Assignment03/ContentView.swift
@@ -20,6 +20,35 @@ struct ContentView: View {
     @State private var totalUsage: String = ""
     
     var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    Toggle("Tiered Pricing? Yes/ No?", isOn: $isTOU)
+                }
+                
+                if isTOU {
+                    // TOU Input Section
+                    Section(header: Text("Tiered Plan Input")) {
+                        TextField("Total usage (kWh)", text: $totalUsage)
+                            .keyboardType(.decimalPad)
+                    }
+                }
+                else {
+                    Section(header: Text("Time-Of-Use Inputs")) {
+                        TextField("On-peak usage (kWh)", text: $onPeak)
+                                .keyboardType(.decimalPad)
+                        TextField("Off-peak usage (kWh)", text: $offPeak)
+                                .keyboardType(.decimalPad)
+                        TextField("Mid-peak usage (kWh)", text: $midPeak)
+                                .keyboardType(.decimalPad)
+                        }
+                    }
+                }
+                // Results Section
+                Section(header: Text("Bill Details")) {
+                    
+                }
+            }
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
