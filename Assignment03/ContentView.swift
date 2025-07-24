aa64d4dca112699ae532e35704fabab6d06650e7
Jashan <jashanjot381@gmail.com>
2025-07-23 23:52:03 -0400

    Testing functionality

--- Assignment03/ContentView.swift
+++ Assignment03/ContentView.swift
@@ -36,29 +36,70 @@ struct ContentView: View {
                 else {
                     Section(header: Text("Time-Of-Use Inputs")) {
                         TextField("On-peak usage (kWh)", text: $onPeak)
-                                .keyboardType(.decimalPad)
+                            .keyboardType(.decimalPad)
                         TextField("Off-peak usage (kWh)", text: $offPeak)
-                                .keyboardType(.decimalPad)
+                            .keyboardType(.decimalPad)
                         TextField("Mid-peak usage (kWh)", text: $midPeak)
-                                .keyboardType(.decimalPad)
-                        }
+                            .keyboardType(.decimalPad)
                     }
                 }
                 // Results Section
                 Section(header: Text("Bill Details")) {
-                    
+                    Text("Consumotion Charges: $\(consumptionCharges, specifier: "%.2f")")
+                    Text("Provincial Rebate (13.1%): $\(provincialRebate, specifier: "%.2f")")
+                    Text("HST (13%): $\(hst, specifier: "%.2f")")
+                    Text("Regulatory Charges: $\(regulatoryCharges, specifier: "%.2f")")
+                    Text("Total Bill: $\(totalBill, specifier: "%.2f")")
+                        .fontWeight(.bold)
                 }
             }
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            .navigationTitle("Energy Bill Calculator")
         }
-        .padding()
+        
+    }
+    
+    // MARK: - Computed Properties
+    
+    var touCharges: Double {
+        let on = Double(onPeak) ?? 0.0
+        let off = Double(offPeak) ?? 0.0
+        let mid = Double(midPeak) ?? 0.0
+        
+        return (on * 0.158) + (off * 0.076) + (mid * 0.122)
+    }
+    
+    var tieredCharges: Double {
+        let usage = Double(totalUsage) ?? 0
+        
+        if usage <= 600 {
+            return usage * 0.093
+        } else {
+            return (600 * 0.093) + ((usage - 600) * 0.11)
+        }
+    }
+    
+    var consumptionCharges: Double {
+        isTOU ? touCharges : tieredCharges
+    }
+    
+    var provincialRebate: Double {
+        0.131 * consumptionCharges
+    }
+    
+    var hst: Double {
+        0.13 * (consumptionCharges - provincialRebate)
+    }
+    
+    var regulatoryCharges: Double {
+        hst - provincialRebate
+    }
+    
+    var totalBill: Double {
+        consumptionCharges + regulatoryCharges
     }
 }
-
+    
 #Preview {
-    ContentView()
+        ContentView()
 }
+    
