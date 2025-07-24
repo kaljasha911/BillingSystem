8d4103f9b25e974ec4fe439924eae663a74438a4
Jashan <jashanjot381@gmail.com>
2025-07-23 21:02:03 -0400

    testing first commit

--- Assignment03/ContentView.swift
+++ Assignment03/ContentView.swift
@@ -8,6 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isTOU: Bool = true
+    
+    // TOU Inputs
+    @State private var onPeak: String = ""
+    @State private var offPeak: String = ""
+    @State private var midPeak: String = ""
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
